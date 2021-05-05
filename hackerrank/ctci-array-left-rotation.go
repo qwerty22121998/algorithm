894076dc0568a97c2da2fc34c739b3774c55b10c
vuhk <vu.ho@onemount.com>
2021-05-05 10:21:29 +0700

    ctci-array-left-rotation.go

---
+++ hackerrank/ctci-array-left-rotation.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+func rotLeft(a []int32, d int32) []int32 {
+    n := len(a)
+    d %= int32(n)
+    var result []int32
+    
+    result = append(result, a[d:]...)
+    result = append(result, a[:d]...)
+    return result
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    dTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+    checkError(err)
+    d := int32(dTemp)
+
+    aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var a []int32
+
+    for i := 0; i < int(n); i++ {
+        aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+        checkError(err)
+        aItem := int32(aItemTemp)
+        a = append(a, aItem)
+    }
+
+    result := rotLeft(a, d)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, " ")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
